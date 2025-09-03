239450ee8fe5769efa6b3aee0dd069b397f87841
SaraBuxbaum <sara3269097@gmail.com>
2025-09-03 16:01:20 +0300

    add classRouter.js

--- server/routes/ClassRouter.js
+++ server/routes/ClassRouter.js
@@ -1,11 +1,18 @@
 import express from 'express';
-import { createClass, getAllClasses } from '../controllers/ClassController.js';
+import { createClass,
+         getAllClasses,
+         updateHomeroomTeacher,
+         removeStudentFromClass,
+         addStudentToClass,
+        } from '../controllers/ClassController.js';
 import { jwtMiddleware } from '../Middlewares.js';
 
 const router = express.Router();
 
 router.post('/createClass', jwtMiddleware, createClass);
 router.get('/', jwtMiddleware, getAllClasses);
-
+router.put('/updateHomeroomTeacher/:name', jwtMiddleware, updateHomeroomTeacher);
+router.post('/addStudent', jwtMiddleware, addStudentToClass);
+router.post('/removeStudent', jwtMiddleware, removeStudentFromClass);
 
 export default router;
