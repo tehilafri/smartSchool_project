2ee0727d9fb1a976665f5cd3ab79943f2e5f8b73
Tehila Friedman <tehila4847@gmail.com>
2025-09-07 09:19:27 +0300

    Updating the schedule model - adding fields

--- server/models/Schedule.js
+++ server/models/Schedule.js
@@ -6,8 +6,11 @@ const lessonSchema = new mongoose.Schema({
   startTime: { type: String, required: true }, // "08:30"
   endTime: { type: String, required: true },   // "09:15"
   substitute: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null },
-  status: { type: String, enum: ['normal','cancelled','replaced'], default: 'normal' }
+  status: { type: String, enum: ['normal','cancelled','replaced'], default: 'normal' },
+  note: { type: String, default: '' },
+  event: { type: mongoose.Schema.Types.ObjectId, ref: 'Event', default: null }
 }, { _id: false });
+
 
 const scheduleSchema = new mongoose.Schema({
   classId: { type: mongoose.Schema.Types.ObjectId, ref: 'Class', required: true },
