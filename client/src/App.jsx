e9db6c0e926e516f4b9cf50fea8b4c64d4ecd0fb
SaraBuxbaum <sara3269097@gmail.com>
2025-10-27 11:26:18 +0200

    push main.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
+import { Provider } from 'react-redux';
+import { store } from './store';
 import Router from "./Router";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Router />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Router />
+      </div>
+    </Provider>
   );
 }
 
