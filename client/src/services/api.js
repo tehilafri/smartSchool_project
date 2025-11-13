17b8184423a8c5a564c0b8c32d9d9d035e6f6341
SaraBuxbaum <sara3269097@gmail.com>
2025-11-13 23:00:15 +0200

    fix api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { showError } from '../components/ErrorNotification';
 
 const api = axios.create({
-  baseURL: process.env.VITE_API_URL,
+  baseURL: `${process.env.VITE_API_URL}/api`,
   headers: {
     'Content-Type': 'application/json',
   },
