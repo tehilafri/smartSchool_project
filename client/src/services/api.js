48b8592aa90ba52b54ba778264b19a2bf70f0bda
Tehila Friedman <tehila4847@gmail.com>
2025-11-16 19:48:35 +0200

    pushed api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 import { showError } from '../components/ErrorNotification';
 
-const SERVER_URL ='8888';
-console.log([...SERVER_URL]);
-console.log('SERVER_URL type:', typeof SERVER_URL);
-console.log('SERVER_URL length:', SERVER_URL?.length);
+const SERVER_URL =import.meta.env.VITE_API_URL ;
 console.log('VITE_API_URL', import.meta.env.VITE_API_URL);
 
 const api = axios.create({
