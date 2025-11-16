db814bf17cffa3c2dd37d780ad30196482a73105
Tehila Friedman <tehila4847@gmail.com>
2025-11-16 19:33:41 +0200

    try

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import { showError } from '../components/ErrorNotification';
 
-const SERVER_URL ='https://smartschool-project-node.onrender.com';
+const SERVER_URL ='8888';
 console.log([...SERVER_URL]);
 console.log('SERVER_URL type:', typeof SERVER_URL);
 console.log('SERVER_URL length:', SERVER_URL?.length);
+console.log('VITE_API_URL', import.meta.env.VITE_API_URL);
 
 const api = axios.create({
   baseURL: `${SERVER_URL}/api`,
