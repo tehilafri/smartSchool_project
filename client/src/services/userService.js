2857c3293f2a5ef70c73817be9a18252fb9d5cb8
Tehila Friedman <tehila4847@gmail.com>
2025-11-18 11:49:25 +0200

    test

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -4,48 +4,48 @@ import api from './api';
 export const loginUser = (userName, password, schoolCode) => {
   const x= "https://smartschool-project-node.onrender.com";
   console.log("❤️",x[0],"❤️");
-  return api.post('/users/login', { userName, password, schoolCode });
+  return api.post('https://smartschool-project-node.onrender.com/api/users/login', { userName, password, schoolCode });
 };
 
 export const registerUser = (userData) => {
-  return api.post('/users/register', userData);
+  return api.post('https://smartschool-project-node.onrender.com/api/users/register', userData);
 };
 
 // User CRUD
 export const getAllTeachers = () => {
-  return api.get('/users/teachers');
+  return api.get('https://smartschool-project-node.onrender.com/api/users/teachers');
 };
 
 export const getAllStudents = () => {
-  return api.get('/users/students');
+  return api.get('https://smartschool-project-node.onrender.com/api/users/students');
 };
 
 export const getUserById = (id) => {
-  return api.get(`/users/${id}`);
+  return api.get(`https://smartschool-project-node.onrender.com/api/users/${id}`);
 };
 
 export const updateUser = (id, updatedData) => {
-  return api.put(`/users/${id}`, updatedData);
+  return api.put(`https://smartschool-project-node.onrender.com/api/users/${id}`, updatedData);
 };
 
 export const deleteUser = (id) => {
-  return api.delete(`/users/${id}`);
+  return api.delete(`https://smartschool-project-node.onrender.com/api/users/${id}`);
 }
 
 // Password management
 export const forgotPassword = (email, userId) => {
-  return api.post('/users/forgot-password', { email, userId });
+  return api.post('https://smartschool-project-node.onrender.com/api/users/forgot-password', { email, userId });
 };
 
 export const resetPassword = (token, newPassword) => {
-  return api.put(`/users/reset-password/${token}`, { password: newPassword });
+  return api.put(`https://smartschool-project-node.onrender.com/api/users/reset-password/${token}`, { password: newPassword });
 };
 
 export const getMe = () => {
-  return api.get('/users/me');
+  return api.get('https://smartschool-project-node.onrender.com/api/users/me');
 };
 
 // getAllSecretaries
 export const getAllSecretaries = () => {
-  return api.get('/users/secretaries');
+  return api.get('https://smartschool-project-node.onrender.com/api/users/secretaries');
 };
